054f075dddb40df36fc3cc75e7b21e57b8f6d5a2
Evans-o <evansopindi@yahoo.com>
2019-12-12 10:56:08 +0300

    added initial structures

--- js/scripts.js
+++ js/scripts.js
@@ -36,5 +36,22 @@ var number2 = parseInt($("#y").val());
         })
      }) 
      
-     //address book
      
+    // business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+$(document).ready(function() {
+  $("#contact").submit(function(event){
+    event.preventDefault();
+    var firstName= $("#firstname").val();
+    var lastName= $("#lastname").val();
+
+    var newContact = new Contact( firstName, lastName);
+    $("ul#contacts").append("<li><span id='contact'>" + newContact.fullName() + "</span></li>");
+  })
+}) 
+Contact.prototype.fullName= function() {
+  return this.firstName + " " + this.lastName;
+}
